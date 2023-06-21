44f9feed281f4ca80bd7b44d71bca77af954b17e
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-06-21 15:04:18 +0300

    value example for useContext

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,11 @@ import React from "react";
 const BlogContext = React.createContext()//created wrapper for the app component
 
 export const BlogProvider = ({children}) =>{//?children is the element which was wrapped. In this example, it's <App/> from App.js
-    <BlogContext.Provider>
+   //?We passed in value to be used for all components
+    return( 
+   <BlogContext.Provider value={'Hello there'}>
         {children}
-    </BlogContext.Provider>
+    </BlogContext.Provider> 
+    )
 }
+export default BlogContext
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React,{useContext} from "react";
 import { StyleSheet,View,Text } from "react-native";
+import BlogContext from "../context/BlogContext";
+
 
 const IndexScreen =()=>{
+    const value = useContext(BlogContext) //?With this, value = the value in BlogContext
     return (
         <View>
             <Text>Index screen</Text>
+            <Text>{value}</Text>
         </View>
     )
 }
