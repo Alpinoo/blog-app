1299142fd549a852f164da203807812e215a2431
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-06-21 15:35:47 +0300

    Moving dynamic data with useContext

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 
 const BlogContext = React.createContext()//created wrapper for the app component
 
 export const BlogProvider = ({children}) =>{//?children is the element which was wrapped. In this example, it's <App/> from App.js
    //?We passed in value to be used for all components
-   const blogPosts = [
-       {title:'Blog Post #1'},
-       {title:'Blog Post #2'}
-    ]
+//    const blogPosts = [
+//        {title:'Blog Post #1'},
+//        {title:'Blog Post #2'}
+//     ]
+    const [blogPost,setBlogPost] = useState([])
+
+    const addBlogPost = ()=>{ //?added this function to be used in other component to trigger adding new post to array
+        setBlogPost([...blogPost,{title:`Blog Post #${blogPost.length+1}`}])
+    }
+
+    //?Now, every component can use this information directly passing from this component
     return( 
-   <BlogContext.Provider value={blogPosts}>
-        {children}
-    </BlogContext.Provider> 
+        <BlogContext.Provider value={{data:blogPost,addBlogPost}}>
+            {children}
+        </BlogContext.Provider> 
     )
 }
 export default BlogContext
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,16 @@
 import React,{useContext} from "react";
-import { StyleSheet,View,Text,FlatList } from "react-native";
+import { StyleSheet,View,Text,FlatList,Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 
 const IndexScreen =()=>{
-    // const value = useContext(BlogContext) //?With this, value = the value in BlogContext
-    const blogPosts = useContext(BlogContext)
+    const {data,addBlogPost} = useContext(BlogContext)
     return (
         <View>
             <Text>Index screen</Text>
+            <Button title="Add Blog" onPress={addBlogPost}/>
             <FlatList
-                data={blogPosts}
+                data={data}
                 keyExtractor={(post)=>post.title}
                 renderItem={({item})=>{
                     return <Text>{item.title}</Text>
