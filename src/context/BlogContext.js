59a42a2a08d1c0df46f317926322d8b2d23abb07
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-06-21 15:11:55 +0300

    passing array to blogContext example

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,8 +4,12 @@ const BlogContext = React.createContext()//created wrapper for the app component
 
 export const BlogProvider = ({children}) =>{//?children is the element which was wrapped. In this example, it's <App/> from App.js
    //?We passed in value to be used for all components
+   const blogPosts = [
+       {title:'Blog Post #1'},
+       {title:'Blog Post #2'}
+    ]
     return( 
-   <BlogContext.Provider value={'Hello there'}>
+   <BlogContext.Provider value={blogPosts}>
         {children}
     </BlogContext.Provider> 
     )
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,21 @@
 import React,{useContext} from "react";
-import { StyleSheet,View,Text } from "react-native";
+import { StyleSheet,View,Text,FlatList } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 
 const IndexScreen =()=>{
-    const value = useContext(BlogContext) //?With this, value = the value in BlogContext
+    // const value = useContext(BlogContext) //?With this, value = the value in BlogContext
+    const blogPosts = useContext(BlogContext)
     return (
         <View>
             <Text>Index screen</Text>
-            <Text>{value}</Text>
+            <FlatList
+                data={blogPosts}
+                keyExtractor={(post)=>post.title}
+                renderItem={({item})=>{
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     )
 }
